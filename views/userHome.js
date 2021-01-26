9032f6dcb62aad8a9237d21103730ba95d283c64
AgustLuj <0j30j2@gmail.com>
2021-01-25 22:31:25 -0300

    Finish

--- views/userHome.js
+++ views/userHome.js
@@ -51,14 +51,6 @@ export default class userHome extends React.Component {
 
                         </View>
                     </View>
-                    <View style={{justifyContent: 'space-around',flexDirection: 'row',}}>
-                        <View style={{width:wp('45%'),height:hp('40%'),backgroundColor:'#171721',}}>
-
-                        </View>
-                        <View style={{width:wp('45%'),height:hp('40%'),backgroundColor:'#171721',}}>
-
-                        </View>
-                    </View>
                 </View>
 
             </View>
